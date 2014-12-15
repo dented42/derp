c2c995f008882f6dde80ce14a44d48049265d40e
Matias Eyzaguirre <matias@fortlogic.net>
2014-12-15 01:18:13 -0700

    Converted everything to #lang modules.

--- fixed-points.rkt
+++ fixed-points.rkt
@@ -1,58 +1,56 @@
-(module fixed-points
-  racket/base
-  
-  (provide define/fix)
+#lang racket/base
 
-  ; Generic tools:
-  (define-syntax while
-    (syntax-rules ()
-      [(_ cond body ...)
-       ; =>
-       (letrec ((lp (λ () (when cond body ... (lp)))))
-         (lp))]))
-  
-  ; Define a recursive (yet monotonic) function over
-  ; a mutually recursive graph by computing its fixed
-  ; point:
-  (define-syntax define/fix
-    (syntax-rules ()
-      [(_ (f x) #:bottom bottom body ...)
-       ; =>
-       (define f (let ((cache     (make-weak-hasheq))
-                       (changed?  (make-parameter 'error-changed))
-                       (running?  (make-parameter #f))
-                       (visited   (make-parameter 'error-visited)))
-                   (λ (x)
-                     (let ((cached? (hash-has-key? cache x))
-                           (cached  (hash-ref cache x (lambda () bottom)))
-                           (run?    (running?)))
-                       (cond
-                         [(and cached? (not run?))
-                          ; =>
-                          cached]
-                         
-                         [(and run? (hash-has-key? (unbox (visited)) x))
-                          ; =>
+(provide define/fix)
+
+; Generic tools:
+(define-syntax while
+  (syntax-rules ()
+    [(_ cond body ...)
+     ; =>
+     (letrec ((lp (λ () (when cond body ... (lp)))))
+       (lp))]))
+
+; Define a recursive (yet monotonic) function over
+; a mutually recursive graph by computing its fixed
+; point:
+(define-syntax define/fix
+  (syntax-rules ()
+    [(_ (f x) #:bottom bottom body ...)
+     ; =>
+     (define f (let ((cache     (make-weak-hasheq))
+                     (changed?  (make-parameter 'error-changed))
+                     (running?  (make-parameter #f))
+                     (visited   (make-parameter 'error-visited)))
+                 (λ (x)
+                   (let ((cached? (hash-has-key? cache x))
+                         (cached  (hash-ref cache x (lambda () bottom)))
+                         (run?    (running?)))
+                     (cond
+                       [(and cached? (not run?))
+                        ; =>
+                        cached]
+                       
+                       [(and run? (hash-has-key? (unbox (visited)) x))
+                        ; =>
                         (if cached? cached bottom)]
-                         
-                         [run? 
-                          ; =>
-                          (hash-set! (unbox (visited)) x #t)
-                          (let ((new-val (begin body ...)))
-                            (when (not (equal? new-val cached))
-                              (set-box! (changed?) #t)
-                              (hash-set! cache x new-val))
-                            new-val)]
-                         
-                         [(and (not cached?) (not run?))
-                          ; =>
-                          (parameterize ([changed? (box #t)]
-                                         [running? #t]
-                                         [visited (box (make-weak-hasheq))])
-                            (let ([v bottom])
-                              (while (unbox (changed?))
-                                     (set-box! (changed?) #f)
-                                     (set-box! (visited) (make-weak-hasheq))
-                                     (set! v (f x)))
-                              v))])))))])))
-  
+                       
+                       [run? 
+                        ; =>
+                        (hash-set! (unbox (visited)) x #t)
+                        (let ((new-val (begin body ...)))
+                          (when (not (equal? new-val cached))
+                            (set-box! (changed?) #t)
+                            (hash-set! cache x new-val))
+                          new-val)]
+                       
+                       [(and (not cached?) (not run?))
+                        ; =>
+                        (parameterize ([changed? (box #t)]
+                                       [running? #t]
+                                       [visited (box (make-weak-hasheq))])
+                          (let ([v bottom])
+                            (while (unbox (changed?))
+                                   (set-box! (changed?) #f)
+                                   (set-box! (visited) (make-weak-hasheq))
+                                   (set! v (f x)))
+                            v))])))))]))
--- lazy-structs.rkt
+++ lazy-structs.rkt
@@ -1,27 +1,25 @@
-(module lazy-structs
-  racket/base
-  
-  (require (for-syntax racket/base))
-  (require racket/match)
-  (require (for-syntax racket/match))
-  (require racket/promise)
-  
-  (provide define-lazy-struct)
-  
-  (define-syntax (define-lazy-struct stx)
-    (syntax-case stx ()
-      [(_ name {field ...})
-       (with-syntax ([$name (datum->syntax #'name (gensym (syntax->datum #'name)))])
-         #'(begin
+#lang racket/base
+
+(require (for-syntax racket/base))
+(require racket/match)
+(require (for-syntax racket/match))
+(require racket/promise)
+
+(provide define-lazy-struct)
+
+(define-syntax (define-lazy-struct stx)
+  (syntax-case stx ()
+    [(_ name {field ...})
+     (with-syntax ([$name (datum->syntax #'name (gensym (syntax->datum #'name)))])
+       #'(begin
+           
+           (define-struct $name {field ...})
+           
+           (define-match-expander name 
+             (syntax-rules () [(_ field ...)
+                               ; =>
+                               ($name (app force field) ...)])
              
-             (define-struct $name {field ...})
-             
-             (define-match-expander name 
-               (syntax-rules () [(_ field ...)
-                                 ; =>
-                                 ($name (app force field) ...)])
-               
-               (syntax-rules () [(_ field ...)
-                                 ; =>
-                                 ($name (delay field) ...)]))))])))
-         
+             (syntax-rules () [(_ field ...)
+                               ; =>
+                               ($name (delay field) ...)]))))]))
